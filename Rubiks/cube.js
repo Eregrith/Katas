e11242ad32b86632572b19f4c964e9e8461d2b65
Stéphane FRACHE <stephane.frache@keyconsulting.fr>
2018-12-11 15:41:15 +0100

    Finished developping the movements, fixed css.

--- Rubiks/cube.js
+++ Rubiks/cube.js
@@ -47,6 +47,28 @@ Cube = {};
         square.classList.add('y'+toY);
     }
 
+    function rotateClockwise(face, offsetX, offsetY) {
+        let F1 = face[0];
+        let F2 = face[1];
+        let F3 = face[2];
+        let F4 = face[3];
+        let F5 = face[4];
+        let F6 = face[5];
+        let F7 = face[6];
+        let F8 = face[7];
+
+        moveFromTo(F1, 1 + offsetX, 1 + offsetY, 3 + offsetX, 1 + offsetY);
+        moveFromTo(F2, 2 + offsetX, 1 + offsetY, 3 + offsetX, 2 + offsetY);
+        moveFromTo(F3, 3 + offsetX, 1 + offsetY, 3 + offsetX, 3 + offsetY);
+
+        moveFromTo(F4, 1 + offsetX, 2 + offsetY, 2 + offsetX, 1 + offsetY);
+        moveFromTo(F5, 3 + offsetX, 2 + offsetY, 2 + offsetX, 3 + offsetY);
+        
+        moveFromTo(F6, 1 + offsetX, 3 + offsetY, 1 + offsetX, 1 + offsetY);
+        moveFromTo(F7, 2 + offsetX, 3 + offsetY, 1 + offsetX, 2 + offsetY);
+        moveFromTo(F8, 3 + offsetX, 3 + offsetY, 1 + offsetX, 3 + offsetY);
+    }
+
     Cube.U = function U() {
         let B1 = document.getElementsByClassName('x1 y4')[0];
         let B2 = document.getElementsByClassName('x2 y4')[0];
@@ -64,16 +86,16 @@ Cube = {};
         let O2 = document.getElementsByClassName('x11 y4')[0];
         let O3 = document.getElementsByClassName('x12 y4')[0];
         
-        let Y1 = document.getElementsByClassName('x4 y1')[0];
-        let Y2 = document.getElementsByClassName('x4 y2')[0];
-        let Y3 = document.getElementsByClassName('x4 y3')[0];
+        let F1 = document.getElementsByClassName('x4 y1')[0];
+        let F2 = document.getElementsByClassName('x5 y1')[0];
+        let F3 = document.getElementsByClassName('x6 y1')[0];
         
-        let Y4 = document.getElementsByClassName('x5 y1')[0];
-        let Y5 = document.getElementsByClassName('x5 y3')[0];
+        let F4 = document.getElementsByClassName('x4 y2')[0];
+        let F5 = document.getElementsByClassName('x6 y2')[0];
 
-        let Y6 = document.getElementsByClassName('x6 y1')[0];
-        let Y7 = document.getElementsByClassName('x6 y2')[0];
-        let Y8 = document.getElementsByClassName('x6 y3')[0];
+        let F6 = document.getElementsByClassName('x4 y3')[0];
+        let F7 = document.getElementsByClassName('x5 y3')[0];
+        let F8 = document.getElementsByClassName('x6 y3')[0];
         
         moveFromTo(B1, 1, 4, 10, 4);
         moveFromTo(B2, 2, 4, 11, 4);
@@ -91,14 +113,8 @@ Cube = {};
         moveFromTo(O2, 11, 4, 8, 4);
         moveFromTo(O3, 12, 4, 9, 4);
 
-        moveFromTo(Y1, 4, 1, 6, 1);
-        moveFromTo(Y2, 4, 2, 5, 1);
-        moveFromTo(Y3, 4, 3, 4, 1);
-        moveFromTo(Y4, 5, 1, 6, 2);
-        moveFromTo(Y5, 5, 3, 4, 2);
-        moveFromTo(Y6, 6, 1, 6, 3);
-        moveFromTo(Y7, 6, 2, 5, 3);
-        moveFromTo(Y8, 6, 3, 4, 3);
+        let face = [F1, F2, F3, F4, F5, F6, F7, F8]
+        rotateClockwise(face, 3, 0);
     }
 
     Cube.U2 = function U2() {
@@ -129,16 +145,16 @@ Cube = {};
         let O2 = document.getElementsByClassName('x10 y5')[0];
         let O3 = document.getElementsByClassName('x10 y6')[0];
         
-        let G1 = document.getElementsByClassName('x7 y4')[0];
-        let G2 = document.getElementsByClassName('x7 y5')[0];
-        let G3 = document.getElementsByClassName('x7 y6')[0];
+        let F1 = document.getElementsByClassName('x7 y4')[0];
+        let F2 = document.getElementsByClassName('x8 y4')[0];
+        let F3 = document.getElementsByClassName('x9 y4')[0];
         
-        let G4 = document.getElementsByClassName('x8 y4')[0];
-        let G5 = document.getElementsByClassName('x8 y6')[0];
+        let F4 = document.getElementsByClassName('x7 y5')[0];
+        let F5 = document.getElementsByClassName('x9 y5')[0];
 
-        let G6 = document.getElementsByClassName('x9 y4')[0];
-        let G7 = document.getElementsByClassName('x9 y5')[0];
-        let G8 = document.getElementsByClassName('x9 y6')[0];
+        let F6 = document.getElementsByClassName('x7 y6')[0];
+        let F7 = document.getElementsByClassName('x8 y6')[0];
+        let F8 = document.getElementsByClassName('x9 y6')[0];
         
         moveFromTo(Y1, 6, 1, 10, 6);
         moveFromTo(Y2, 6, 2, 10, 5);
@@ -156,14 +172,8 @@ Cube = {};
         moveFromTo(O2, 10, 5, 6, 8);
         moveFromTo(O3, 10, 6, 6, 7);
 
-        moveFromTo(G1, 7, 4, 9, 4);
-        moveFromTo(G2, 7, 5, 8, 4);
-        moveFromTo(G3, 7, 6, 7, 4);
-        moveFromTo(G4, 8, 4, 9, 5);
-        moveFromTo(G5, 8, 6, 7, 5);
-        moveFromTo(G6, 9, 4, 9, 6);
-        moveFromTo(G7, 9, 5, 8, 6);
-        moveFromTo(G8, 9, 6, 7, 6);
+        let face = [F1, F2, F3, F4, F5, F6, F7, F8]
+        rotateClockwise(face, 6, 3);
     }
 
     Cube.R2 = function R2() {
@@ -194,16 +204,16 @@ Cube = {};
         let O2 = document.getElementsByClassName('x12 y5')[0];
         let O3 = document.getElementsByClassName('x12 y6')[0];
         
-        let B1 = document.getElementsByClassName('x1 y4')[0];
-        let B2 = document.getElementsByClassName('x1 y5')[0];
-        let B3 = document.getElementsByClassName('x1 y6')[0];
+        let F1 = document.getElementsByClassName('x1 y4')[0];
+        let F2 = document.getElementsByClassName('x2 y4')[0];
+        let F3 = document.getElementsByClassName('x3 y4')[0];
         
-        let B4 = document.getElementsByClassName('x2 y4')[0];
-        let B5 = document.getElementsByClassName('x2 y6')[0];
+        let F4 = document.getElementsByClassName('x1 y5')[0];
+        let F5 = document.getElementsByClassName('x3 y5')[0];
 
-        let B6 = document.getElementsByClassName('x3 y4')[0];
-        let B7 = document.getElementsByClassName('x3 y5')[0];
-        let B8 = document.getElementsByClassName('x3 y6')[0];
+        let F6 = document.getElementsByClassName('x1 y6')[0];
+        let F7 = document.getElementsByClassName('x2 y6')[0];
+        let F8 = document.getElementsByClassName('x3 y6')[0];
         
         moveFromTo(Y1, 4, 1, 4, 4);
         moveFromTo(Y2, 4, 2, 4, 5);
@@ -221,14 +231,8 @@ Cube = {};
         moveFromTo(O2, 12, 5, 4, 2);
         moveFromTo(O3, 12, 6, 4, 1);
 
-        moveFromTo(B1, 1, 4, 3, 4);
-        moveFromTo(B2, 1, 5, 2, 4);
-        moveFromTo(B3, 1, 6, 1, 4);
-        moveFromTo(B4, 2, 4, 3, 5);
-        moveFromTo(B5, 2, 6, 1, 5);
-        moveFromTo(B6, 3, 4, 3, 6);
-        moveFromTo(B7, 3, 5, 2, 6);
-        moveFromTo(B8, 3, 6, 1, 6);
+        let face = [F1, F2, F3, F4, F5, F6, F7, F8]
+        rotateClockwise(face, 0, 3);
     }
 
     Cube.L2 = function L2() {
@@ -240,6 +244,207 @@ Cube = {};
         Cube.L();
         Cube.L();
         Cube.L();
+    }
+
+    Cube.F = function F() {
+        let B1 = document.getElementsByClassName('x3 y4')[0];
+        let B2 = document.getElementsByClassName('x3 y5')[0];
+        let B3 = document.getElementsByClassName('x3 y6')[0];
+        
+        let Y1 = document.getElementsByClassName('x4 y3')[0];
+        let Y2 = document.getElementsByClassName('x5 y3')[0];
+        let Y3 = document.getElementsByClassName('x6 y3')[0];
+        
+        let G1 = document.getElementsByClassName('x7 y4')[0];
+        let G2 = document.getElementsByClassName('x7 y5')[0];
+        let G3 = document.getElementsByClassName('x7 y6')[0];
+        
+        let W1 = document.getElementsByClassName('x4 y7')[0];
+        let W2 = document.getElementsByClassName('x5 y7')[0];
+        let W3 = document.getElementsByClassName('x6 y7')[0];
+        
+        let F1 = document.getElementsByClassName('x4 y4')[0];
+        let F2 = document.getElementsByClassName('x5 y4')[0];
+        let F3 = document.getElementsByClassName('x6 y4')[0];
+        
+        let F4 = document.getElementsByClassName('x4 y5')[0];
+        let F5 = document.getElementsByClassName('x6 y5')[0];
+
+        let F6 = document.getElementsByClassName('x4 y6')[0];
+        let F7 = document.getElementsByClassName('x5 y6')[0];
+        let F8 = document.getElementsByClassName('x6 y6')[0];
+        
+        moveFromTo(B1, 3, 4, 6, 3);
+        moveFromTo(B2, 3, 5, 5, 3);
+        moveFromTo(B3, 3, 6, 4, 3);
+
+        moveFromTo(Y1, 4, 3, 7, 4);
+        moveFromTo(Y2, 5, 3, 7, 5);
+        moveFromTo(Y3, 6, 3, 7, 6);
+
+        moveFromTo(G1, 7, 4, 6, 7);
+        moveFromTo(G2, 7, 5, 5, 7);
+        moveFromTo(G3, 7, 6, 4, 7);
+
+        moveFromTo(W1, 4, 7, 3, 4);
+        moveFromTo(W2, 5, 7, 3, 5);
+        moveFromTo(W3, 6, 7, 3, 6);
+
+        let face = [F1, F2, F3, F4, F5, F6, F7, F8]
+        rotateClockwise(face, 3, 3);
+    }
+
+    Cube.F2 = function F2() {
+        Cube.F();
+        Cube.F();
+    }
+
+    Cube.Fprime = function Fprime() {
+        Cube.F();
+        Cube.F();
+        Cube.F();
+    }
+
+    Cube.D = function D() {
+        let B1 = document.getElementsByClassName('x1 y6')[0];
+        let B2 = document.getElementsByClassName('x2 y6')[0];
+        let B3 = document.getElementsByClassName('x3 y6')[0];
+        
+        let R1 = document.getElementsByClassName('x4 y6')[0];
+        let R2 = document.getElementsByClassName('x5 y6')[0];
+        let R3 = document.getElementsByClassName('x6 y6')[0];
+        
+        let G1 = document.getElementsByClassName('x7 y6')[0];
+        let G2 = document.getElementsByClassName('x8 y6')[0];
+        let G3 = document.getElementsByClassName('x9 y6')[0];
+        
+        let O1 = document.getElementsByClassName('x10 y6')[0];
+        let O2 = document.getElementsByClassName('x11 y6')[0];
+        let O3 = document.getElementsByClassName('x12 y6')[0];
+        
+        let F1 = document.getElementsByClassName('x4 y7')[0];
+        let F2 = document.getElementsByClassName('x5 y7')[0];
+        let F3 = document.getElementsByClassName('x6 y7')[0];
+        
+        let F4 = document.getElementsByClassName('x4 y8')[0];
+        let F5 = document.getElementsByClassName('x6 y8')[0];
+
+        let F6 = document.getElementsByClassName('x4 y9')[0];
+        let F7 = document.getElementsByClassName('x5 y9')[0];
+        let F8 = document.getElementsByClassName('x6 y9')[0];
+        
+        moveFromTo(B1, 1, 6, 4, 6);
+        moveFromTo(B2, 2, 6, 5, 6);
+        moveFromTo(B3, 3, 6, 6, 6);
+
+        moveFromTo(R1, 4, 6, 7, 6);
+        moveFromTo(R2, 5, 6, 8, 6);
+        moveFromTo(R3, 6, 6, 9, 6);
+
+        moveFromTo(G1, 7, 6, 10, 6);
+        moveFromTo(G2, 8, 6, 11, 6);
+        moveFromTo(G3, 9, 6, 12, 6);
+
+        moveFromTo(O1, 10, 6, 1, 6);
+        moveFromTo(O2, 11, 6, 2, 6);
+        moveFromTo(O3, 12, 6, 3, 6);
+
+        let face = [F1, F2, F3, F4, F5, F6, F7, F8]
+        rotateClockwise(face, 3, 6);
+    }
+
+    Cube.D2 = function D2() {
+        Cube.D();
+        Cube.D();
+    }
+
+    Cube.Dprime = function Dprime() {
+        Cube.D();
+        Cube.D();
+        Cube.D();
+    }
+
+    Cube.B = function B() {
+        let G1 = document.getElementsByClassName('x9 y4')[0];
+        let G2 = document.getElementsByClassName('x9 y5')[0];
+        let G3 = document.getElementsByClassName('x9 y6')[0];
+        
+        let Y1 = document.getElementsByClassName('x4 y1')[0];
+        let Y2 = document.getElementsByClassName('x5 y1')[0];
+        let Y3 = document.getElementsByClassName('x6 y1')[0];
+        
+        let B1 = document.getElementsByClassName('x1 y4')[0];
+        let B2 = document.getElementsByClassName('x1 y5')[0];
+        let B3 = document.getElementsByClassName('x1 y6')[0];
+        
+        let W1 = document.getElementsByClassName('x4 y9')[0];
+        let W2 = document.getElementsByClassName('x5 y9')[0];
+        let W3 = document.getElementsByClassName('x6 y9')[0];
+        
+        let F1 = document.getElementsByClassName('x10 y4')[0];
+        let F2 = document.getElementsByClassName('x11 y4')[0];
+        let F3 = document.getElementsByClassName('x12 y4')[0];
+        
+        let F4 = document.getElementsByClassName('x10 y5')[0];
+        let F5 = document.getElementsByClassName('x12 y5')[0];
+
+        let F6 = document.getElementsByClassName('x10 y6')[0];
+        let F7 = document.getElementsByClassName('x11 y6')[0];
+        let F8 = document.getElementsByClassName('x12 y6')[0];
+        
+        moveFromTo(G1, 9, 4, 4, 1);
+        moveFromTo(G2, 9, 5, 5, 1);
+        moveFromTo(G3, 9, 6, 6, 1);
+
+        moveFromTo(Y1, 4, 1, 1, 6);
+        moveFromTo(Y2, 5, 1, 1, 5);
+        moveFromTo(Y3, 6, 1, 1, 4);
+
+        moveFromTo(B1, 1, 4, 4, 9);
+        moveFromTo(B2, 1, 5, 5, 9);
+        moveFromTo(B3, 1, 6, 6, 9);
+
+        moveFromTo(W1, 4, 9, 9, 6);
+        moveFromTo(W2, 5, 9, 9, 5);
+        moveFromTo(W3, 6, 9, 9, 4);
+
+        let face = [F1, F2, F3, F4, F5, F6, F7, F8];
+        rotateClockwise(face, 9, 3);
+    }
+
+    Cube.B2 = function B2() {
+        Cube.B();
+        Cube.B();
+    }
+
+    Cube.Bprime = function Bprime() {
+        Cube.B();
+        Cube.B();
+        Cube.B();
+    }
+
+    function rand(from, to) {
+        return Math.floor((Math.random() * to) + from);
+    }
+
+    Cube.useRandomMovement = function useRandomMovement() {
+        let movements = [ Cube.U,Cube.U2,Cube.Uprime,
+                          Cube.D,Cube.D2,Cube.Dprime,
+                          Cube.R,Cube.R2,Cube.Rprime,
+                          Cube.L,Cube.L2,Cube.Lprime,
+                          Cube.F,Cube.F2,Cube.Fprime,
+                          Cube.B,Cube.B2,Cube.Bprime ];
+        let num = movements.length;
+
+        return movements[rand(0, num)]();
+    }
+
+    Cube.shuffle = function shuffle() {
+        let shuffleCount = 30;
+        
+        for (i = 0; i < shuffleCount; i++) {
+            setTimeout(Cube.useRandomMovement, 250 * i);
+        }
     }
 
 })(Cube);
